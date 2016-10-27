d8a78a503d86e38e752304725a3472a89b9bd50b
Rey Garcia <reynaldogarcia@Reynaldos-iMac.local>
2016-10-27 14:42:59 -0600

    react

---
+++ src/app/Todo.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+
+
+export class Todo extends Component {
+    constructor() {
+        super();
+        this.state = {
+           todos:[],
+           newTodo:''
+        }
+    }
+
+    handleChange (e) {
+  		const {value} = e.target;
+  		this.setState({newTodo: value}); 
+    }
+
+      handleClick(e) {
+  		e.preventDefault();
+  		const todos = [...this.state.todos, this.input.value];
+  		this.input.value = '';
+  		this.setState({todos});
+    }
+
+    removeTodo(i) {
+    	const todos = [this.state.todos.slice(0, i), this.state.todos.slice(i + 1)]
+    	this.setState({todos});
+    }
+
+    render() {
+            return ( <div>
+            	<form>		
+            		<input 
+            		ref={node => this.input = node}
+            		type="text" placeholder="new todo"/>
+            		<button onClick={this.handleClick.bind(this)}>create</button>
+            	</form>
+            	<ul>
+            		{this.state.todos.map((todo, i) => (
+            			<li onClick={() => this.removeTodo.call(this, i)}>{todo}</li>
+            			))}
+            	</ul>
+                        </div>
+                         
+
+                            );
+                        }
+                    }
